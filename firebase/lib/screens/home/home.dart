c1a0fd12b9f548130ddcf52f0ffcb6f06aaf41aa
KHP53 <khp4121@gmail.com>
2022-10-22 19:10:36 +0600

    added task adding and updating

--- firebase/lib/screens/home/home.dart
+++ firebase/lib/screens/home/home.dart
@@ -11,6 +11,9 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _services = FirebaseDatabase();
   Stream? brandStream;
+  Stream? todoStream;
+
+  final _addController = TextEditingController();
 
   getBrands() {
     setState(() {
@@ -18,10 +21,17 @@ class _HomeState extends State<Home> {
     });
   }
 
+  getUserTodo() {
+    setState(() {
+      todoStream = _services.getTodoFoUser();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     getBrands();
+    getUserTodo();
   }
 
   @override
@@ -33,27 +43,129 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         margin: const EdgeInsets.all(25),
-        child: StreamBuilder(
-          stream: brandStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(snapshot.data!.docs[index]['name']),
-                    subtitle: Text(snapshot.data!.docs[index]['industry']),
-                  );
+        child: taskList(),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showModalBottomSheet(
+          context: context,
+          builder: (context) => Container(
+            margin: const EdgeInsets.all(30),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _addController,
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return "This field cannot be empty";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Add Tasks",
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  minWidth: MediaQuery.of(context).size.width,
+                  onPressed: () {
+                    var res = _services.addTasks(_addController.text.trim());
+                    res
+                        .then(
+                          (val) => ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Task Added!"),
+                            ),
+                          ),
+                        )
+                        .catchError(
+                          (error) => ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(error.toString()),
+                            ),
+                          ),
+                        );
+                    _addController.clear();
+                    Navigator.pop(context);
+                  },
+                  color: Colors.lightBlue,
+                  child: const Text("Add you tasks!"),
+                ),
+              ],
+            ),
+          ),
+        ),
+        label: const Text("Add Tasks"),
+      ),
+    );
+  }
+
+  StreamBuilder<dynamic> homeList() {
+    return StreamBuilder(
+      stream: brandStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data!.docs[index]['name']),
+                subtitle: Text(snapshot.data!.docs[index]['industry']),
+              );
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  StreamBuilder<dynamic> taskList() {
+    return StreamBuilder(
+      stream: todoStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return CheckboxListTile(
+                title: Text(snapshot.data!.docs[index]['title']),
+                activeColor: Colors.green,
+                checkColor: Colors.white,
+                selected: snapshot.data!.docs[index]['isCompleted'],
+                value: snapshot.data!.docs[index]['isCompleted'],
+                onChanged: (value) {
+                  var res = _services.completeTask(
+                      true, snapshot.data!.docs[index].id);
+                  res
+                      .then(
+                        (val) => ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Task Completed!"),
+                          ),
+                        ),
+                      )
+                      .catchError(
+                        (error) => ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(error.toString()),
+                          ),
+                        ),
+                      );
                 },
               );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      ),
+            },
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
--- firebase/lib/services/firebase_database.dart
+++ firebase/lib/services/firebase_database.dart
@@ -1,9 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseDatabase {
   final _services = FirebaseFirestore.instance;
+  final _fauth = FirebaseAuth.instance;
 
   getBrands() {
     return _services.collection('brands').snapshots();
+  }
+
+  getTodoFoUser() {
+    return _services
+        .collection('todo')
+        .doc(_fauth.currentUser!.uid)
+        .collection('tasks')
+        .snapshots();
+  }
+
+  completeTask(bool isCompleted, String docId) {
+    return _services
+        .collection('todo')
+        .doc(_fauth.currentUser!.uid)
+        .collection('tasks')
+        .doc(docId)
+        .update({
+      'isCompleted': isCompleted,
+    });
+  }
+
+  addTasks(String title) {
+    return _services
+        .collection('todo')
+        .doc(_fauth.currentUser!.uid)
+        .collection('tasks')
+        .doc()
+        .set({
+      'title': title,
+      'isCompleted': false,
+    });
   }
 }
